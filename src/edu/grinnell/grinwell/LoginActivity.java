265f877a3485f63157e9c0c966e835284d2db9b8
Michael Owusu <mkowusu@gmail.com>
2014-09-13 20:38:32 -0500

    initial commit, no games

---
+++ src/edu/grinnell/grinwell/LoginActivity.java
@@ -0,0 +1,29 @@
+package edu.grinnell.grinwell;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class LoginActivity extends ActionBarActivity {
+	
+	Button loginButton, signupButton;
+	
+	TextView emailTextView, passwordTextView; 
+
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_view);
+		
+		
+	
+	}
+		
+	
+	
+	
+	
+
+}
